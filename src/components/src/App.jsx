df104e5a55ce979021debe7ef5fe1f03b99ce4da
rafaalh7 <leyvarafael542@gmail.com>
2025-05-13 12:08:32 -0400

    Create App.jsx

---
+++ src/components/src/App.jsx
@@ -0,0 +1,12 @@
+import Mapa from './components/Mapa'
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Mapa Interactivo</h1>
+      <Mapa />
+    </div>
+  )
+}
+
+export default App
